ab90c7bb9ba1021373495077c062dc173b637065
Zaewei <roberttrinidad18@gmail.com>
2026-06-08 23:32:50 +0800

    absolute directory for db file

--- IM/ResPAWnsible/index.py
+++ IM/ResPAWnsible/index.py
@@ -1,6 +1,9 @@
 import sqlite3
+import os
 
-conn = sqlite3.connect('respawnsible.db')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, 'respawnsible.db')
+conn = sqlite3.connect(db_path)
 c = conn.cursor()
 
 c.executescript('''
